e0cfd6c929b25f027e0c32063e101171e63bf2c5
reutki <marcu.reutki@gmail.com>
2022-12-12 14:15:03 +0200

    added millify for more readable data

--- components/CoinSimpleScheme/index.jsx
+++ components/CoinSimpleScheme/index.jsx
@@ -3,25 +3,16 @@ import { CoinStyledScheme } from './styled'
 import { Avatar, Typography, Card, CardActionArea, Collapse, Button, IconButton, CardContent } from '@mui/material'
 import { ExpandMore } from '@mui/icons-material'
 import { styled } from '@mui/material/styles';
+import millify from 'millify';
 
 
 export const CoinSimpleScheme = ({ icon, name, code, price, chart, precentage, rank }) => {
     const newPrice = Number(price).toFixed(2)
     const [expanded, setExpanded] = useState(false);
-    const ExpandMoreButton = styled((props) => {
-        const { expand, ...other } = props;
-        return <IconButton {...other} />;
-    })(({ theme, expand }) => ({
-        transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-        }),
-    }));
 
     const handleExpandClick = () => { setExpanded(!expanded) };
     return (
-        < CoinStyledScheme >
+        <CoinStyledScheme>
             <Card onClick={handleExpandClick}>
                 <CardActionArea>
                     <Avatar src={icon} alt='coin logo' />
@@ -32,13 +23,13 @@ export const CoinSimpleScheme = ({ icon, name, code, price, chart, precentage, r
                     </div>
                     <Typography>Chart</Typography>
                     <div className='price'>
-                        <Typography>{newPrice}$</Typography>
-                        <Typography>{precentage}%</Typography>
+                        <Typography>{millify(newPrice)}$</Typography>
+                        <Typography>{millify(precentage)}%</Typography>
                     </div>
                 </CardActionArea>
                 <Collapse in={expanded} timeout="auto" unmountOnExit>
                     <CardContent>
-                        <Typography >Chart Here</Typography>
+                        <Typography>Chart Here</Typography>
                     </CardContent>
                 </Collapse>
             </Card >
