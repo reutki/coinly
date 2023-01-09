a2a41ef41a5320365d2ce51077eefe3dee883c5b
reutki <marcu.reutki@gmail.com>
2023-01-09 19:16:05 +0200

    [Profile]: added css

--- components/Profile/index.jsx
+++ components/Profile/index.jsx
@@ -1,43 +1,41 @@
 import React from 'react'
-import { Paper, Avatar, Typography, Switch } from '@mui/material'
+import { Paper, Avatar, Typography, Switch, Card, Button } from '@mui/material'
 import SettingsIcon from '@mui/icons-material/Settings';
 import HelpIcon from '@mui/icons-material/Help';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import { AvatarPaper, PortofolioPaper } from './styled'
+import { AvatarPaper, PortofolioPaper, ProfileStyle, Balance } from './styled'
 
 export const Profile = () => {
   return (
-    <>
+    <ProfileStyle>
       <AvatarPaper>
-
         <Paper className='profile_paper'>
           <Avatar className='avatar_icon' />
-          <Typography>Reutki Marcu</Typography>
-          <Typography>marcu.reutki@gmail.com</Typography>
+          <Typography variant='body1' className='profile_name'>Reutki Marcu</Typography>
         </Paper>
       </AvatarPaper>
       <PortofolioPaper>
 
         <Paper className='portofolio_paper'>
-          <>
-            <Typography className='portofolio_paper'>Portofolio</Typography>
+          <Typography >Balance</Typography>
+          <Balance>
+            <Typography>0</Typography>
             <AttachMoneyIcon />
-          </>
-          <Typography>0$</Typography>
+          </Balance>
         </Paper>
       </PortofolioPaper>
-      <>
+      <Card className='card_option'>
         <SettingsIcon />
         <Typography>Settings</Typography>
-      </>
-      <>
+      </Card >
+      <Card className='card_option'>
         <Switch />
         <Typography>Theme</Typography>
-      </>
-      <>
+      </Card >
+      <Card className='card_option'>
         <HelpIcon />
         <Typography>Help and Contact</Typography>
-      </>
-    </>
+      </Card>
+    </ProfileStyle>
   )
 }
--- components/Profile/styled.jsx
+++ components/Profile/styled.jsx
@@ -1,17 +1,40 @@
 import styled from '@emotion/styled';
 
+export const ProfileStyle = styled.div`
+    max-width: 450px;
+    margin: 0 auto;
+    .card_option{
+        display: grid;
+        grid-template-columns: max-content max-content;
+        padding: 20px;
+        align-items: center;
+        column-gap: 7px;
+        margin-bottom: 10px;
+        height: 30px;
+    }
+`
+
 export const AvatarPaper = styled.div`
 padding: 5px 3px;
 .profile_paper{
-    background-color: #0063F5;
+    background-color: #181818;
     color: white;
     display: grid;
+    grid-template-columns: max-content max-content;
     justify-content: center;
     align-items: center;
     text-align: center;
     padding: 15px 0;
 }
+.profile_name{
+margin-left: 15px;
+}
 
+`
+export const Balance = styled.div`
+display: grid;
+grid-template-columns: max-content max-content;
+justify-content: center;
 `
 export const PortofolioPaper = styled.div`
 padding: 5px 3px;
@@ -21,6 +44,8 @@ padding: 5px 3px;
     display: grid;
     justify-content: center;
     padding: 15px 0;
+margin-bottom: 30px;
+
 }
 
 `
